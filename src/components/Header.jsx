1547bbebecb7990adc2680495d2e896516505e38
xZaaaM <25784985+KiMoOxD@users.noreply.github.com>
2024-08-18 16:00:32 +0300

    gelpp

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ import AccountInfo from "./AccountInfo";
 
 
 export default function Header() {
-  let { currentUser, logout } = useAuth()
+  let { currentUser } = useAuth()
   return (
     <div className="flex sticky h-12 top-0 z-40 mx-auto justify-between items-center px-4 2xl:px-0">
       <Link to={`/`}>
