3117a4caf4cb95f7a78adcb965decee2259e6cf5
xZaaaM <25784985+KiMoOxD@users.noreply.github.com>
2024-08-25 17:14:49 +0300

    updates

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,35 +7,69 @@ export default function Home() {
   return (
     <div>
       <section className=" text-white min-h-[90vh] flex items-center justify-center">
-        <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8 lg:py-16">
-          <div className="mx-auto relative max-w-lg text-center">
-            <div className="flex justify-center items-center">
-            </div>
-            <div className="overflow-hidden relative h-[200px]">
-            <img src={taxiimg} alt="Taxi Animation" className="w-full object-cover" />
-            </div>
-            <h2 className="w-[500px] text-3xl font-bold sm:text-4xl">
-              Embark on Your Adventure
-            </h2>
-            <TextEffect per="char" preset="fade" className="mt-4 text-gray-300">
+        <div className="flex flex-col text-center lg:flex-row lg:text-start px-5 py-10 lg:justify-between items-center gap-10 mx-auto max-w-screen-2xl w-full">
+          <div className="order-1 lg:order-0 mx-auto relative max-w-3xl">
+            {/* <div className="overflow-hidden relative h-[200px]">
+              <img
+                src={taxiimg}
+                alt="Taxi Animation"
+                className="w-full absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%]"
+              />
+            </div> */}
+            <p
+              per="char"
+              preset="fade"
+              className=" text-3xl font-bold sm:text-7xl"
+            >
+              Embark on Your <span className="text-yellow-400 drop-shadow-[0_0_30px_rgba(250,204,21,0.5)]">Adventure</span>
+            </p>
+            <TextEffect
+              per="char"
+              preset="fade"
+              className="mt-4 text-gray-300 text-2xl"
+            >
               Ready to explore the world?
             </TextEffect>
-            <TextEffect per="char" preset="fade" className="mt-4 text-gray-300">
+            <TextEffect
+              per="char"
+              preset="fade"
+              className="mt-4 text-gray-300 text-md"
+            >
               Our platform offers personalized travel experiences tailored to
               your unique desires. From serene beach getaways to thrilling city
               adventures, we've got the perfect itinerary waiting for you.
               Embark on a journey of discovery and create unforgettable
               memories.
             </TextEffect>
+            <div className="mt-8">
+              <Link
+                to={"/predict"}
+                className="inline-block rounded bg-transparent px-12 py-3 text-sm font-medium text-yellow-400 border shadow-xl border-yellow-400 transition hover:shadow-yellow-500/50 hover:bg-yellow-400/80 hover:text-black focus:outline-none focus:ring focus:ring-yellow-400"
+              >
+                Get Started Today
+              </Link>
+            </div>
           </div>
 
-          <div className="mt-12 text-center">
-            <Link
-              to={"/predict"}
-              className="inline-block rounded bg-pink-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-pink-700 focus:outline-none focus:ring focus:ring-yellow-400"
-            >
-              Get Started Today
-            </Link>
+          <div class="relative drop-shadow-[0_0_10px_rgba(250,204,255,0.1)] mx-auto lg:mr-auto lg:ml-0 order-0 lg:order-1 border-gray-800 bg-gray-800 border-[14px] rounded-[2.5rem] h-[550px] w-[290px] shadow-xl">
+            <div class="z-10 w-[148px] h-[18px] bg-gray-800 top-0 rounded-b-[1rem] left-1/2 -translate-x-1/2 absolute"></div>
+            <div class="z-10 h-[46px] w-[3px] bg-gray-800 absolute -start-[17px] top-[124px] rounded-s-lg"></div>
+            <div class="z-10 h-[46px] w-[3px] bg-gray-800 absolute -start-[17px] top-[178px] rounded-s-lg"></div>
+            <div class="z-10 h-[64px] w-[3px] bg-gray-800 absolute -end-[17px] top-[142px] rounded-e-lg"></div>
+            <div class="rounded-[2rem] overflow-hidden w-full h-full bg-white dark:bg-gray-800">
+              <div className="overflow-hidden absolute top-0 w-full rounded-[2.5rem] bg-[#374151] h-[200px]">
+                <img
+                  src={taxiimg}
+                  alt="Taxi Animation"
+                  className="w-full absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%]"
+                />
+              </div>
+              <img
+                src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/hero/mockup-1-dark.png"
+                className="block w-full h-full"
+                alt=""
+              />
+            </div>
           </div>
         </div>
       </section>
