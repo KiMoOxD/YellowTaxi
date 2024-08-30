c13fc4aec3a63fd6c1f01c264d4871ac219a5853
xZaaaM <25784985+KiMoOxD@users.noreply.github.com>
2024-08-30 16:54:42 +0300

    optimized animations 1.2.1

--- src/components/menu.jsx
+++ src/components/menu.jsx
@@ -20,7 +20,7 @@ export default function Menu() {
             animate={{ opacity: 1, scaleX: 1 }}
             exit={{ opacity: 0, scaleX: 0.95 }}
             transition={{ duration: 0}}
-            className="h-[95vh] trans origin-right w-[100vw] *:w-full *:text-center md:w-96 absolute top-[48px] right-0 pt-14 flex flex-col gap-2 border border-stone-50/10 bg-stone-900 rounded-sm"
+            className="h-[calc(100vh_-_48px)] origin-right w-[100vw] *:w-full *:text-center md:w-96 absolute top-[34px] -right-4 pt-14 flex flex-col gap-2 border border-stone-50/10 bg-stone-900 rounded-sm"
           >
             <div
               className="text-white text-2xl absolute top-5 left-5"
@@ -47,7 +47,7 @@ export default function Menu() {
         )}
       </AnimatePresence>
       {isMenuOpen && (
-        <div className="top-8 right-5 animate-bounce border-b-4 border-r-4 border-l-4 border-b-stone-50 border-transparent bg-transparent size-2.5 bg-slate-800 absolute"></div>
+        <div className="top-5 right-1 animate-bounce border-b-4 border-r-4 border-l-4 border-b-stone-50 border-transparent bg-transparent size-2.5 bg-slate-800 absolute"></div>
       )}
     </div>
   );
