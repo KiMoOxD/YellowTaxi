9a6534b5f01a6c77a7e12fefa46089f2a40e237c
xZaaaM <25784985+KiMoOxD@users.noreply.github.com>
2024-08-30 17:34:51 +0300

    optimized animations 1.2.1

--- src/components/menu.jsx
+++ src/components/menu.jsx
@@ -3,7 +3,7 @@ import { IoMdExit } from "react-icons/io";
 import { NavLink } from "react-router-dom";
 import { IoMdClose } from "react-icons/io";
 import { useGeneralContext } from "../context/GeneralContext";
-import { AnimatePresence, motion } from "framer-motion";
+import { motion } from "framer-motion";
 
 export default function Menu() {
   let { isMenuOpen, toggleMenu } = useGeneralContext()
@@ -13,14 +13,14 @@ export default function Menu() {
     <div className="lg:hidden text-xl cursor-pointer">
       {!isMenuOpen && <CiMenuBurger onClick={toggleMenu} />}
       {isMenuOpen && <IoMdClose onClick={toggleMenu} />}
-      <AnimatePresence>
+      {/* <AnimatePresence> */}
         {isMenuOpen && (
           <motion.div
-            initial={{ opacity: 0, x: '20px' }}
+            initial={{ opacity: 0.5, x: '20px' }}
             animate={{ opacity: 1, x: '0' }}
-            exit={{ opacity: 0, x: '20px' }}
-            transition={{ duration: 0.3, ease: 'easeOut'}}
-            className="h-[calc(100vh_-_48px)] origin-right w-[100vw] *:w-full *:text-center md:w-96 absolute top-[48px] right-0 pt-14 flex flex-col gap-2 border border-stone-50/10 bg-stone-900 rounded-sm"
+            exit={{ opacity: 0.5, x: '20px' }}
+            transition={{ duration: 0.3}}
+            className="h-[calc(100vh_-_48px)] origin-right w-full *:w-full *:text-center md:w-96 absolute top-[48px] right-0 pt-14 flex flex-col gap-2 border border-stone-50/10 bg-stone-900 rounded-sm"
           >
             <div
               className="text-white text-2xl absolute top-5 left-5"
@@ -45,7 +45,7 @@ export default function Menu() {
             </NavLink>
           </motion.div>
         )}
-      </AnimatePresence>
+      {/* </AnimatePresence> */}
       {isMenuOpen && (
         <div className="top-8 right-5 animate-bounce border-b-4 border-r-4 border-l-4 border-b-stone-50 border-transparent bg-transparent size-2.5 bg-slate-800 absolute"></div>
       )}
